bf9edfe109552bd0199dea8ce68fc2806d7641a9
Neil Trini Lasrado <neil@frappe.io>
2015-04-28 20:24:25 +0530

    permissions managed for dcm, project - auto calculation of attenndence added

--- pranali_reloaded/pranali_reloaded/doctype/project/project.py
+++ pranali_reloaded/pranali_reloaded/doctype/project/project.py
@@ -3,8 +3,12 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe.model.document import Document
 
 class Project(Document):
-	pass
+	def validate(self):
+		self.calculate_totals()
+		
+	def calculate_totals(self):
+		self.total = self.home_club + self.other_club + self.dcm \
+			+ self.alumini + self.rotarians + self.pis + self.guest
