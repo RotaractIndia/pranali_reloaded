202299d3d021d8db67478fd015517ca920ff10bf
Neil Trini Lasrado <neil@frappe.io>
2015-05-06 23:52:13 +0530

    updates

--- pranali_reloaded/pranali_reloaded/doctype/avenue/avenue.py
+++ pranali_reloaded/pranali_reloaded/doctype/avenue/avenue.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2015, Rtr.Neil Trini Lasrado and contributors
 # For license information, please see license.txt
 
@@ -5,9 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class Member(Document):
-	def validate(self):
-		self.set_zone()
-		
-	def set_zone(self):
-		self.zone = frappe.db.get_value("Club", self.club, "zone")
+class Avenue(Document):
+	pass
--- pranali_reloaded/pranali_reloaded/doctype/project/project.py
+++ pranali_reloaded/pranali_reloaded/doctype/project/project.py
@@ -3,12 +3,18 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
+from frappe.utils import cint
 from frappe.model.document import Document
 
 class Project(Document):
 	def validate(self):
 		self.calculate_totals()
+		self.set_zone()
 		
 	def calculate_totals(self):
-		self.total = self.home_club + self.other_club + self.dcm \
-			+ self.alumini + self.rotarians + self.pis + self.guest
+		self.total = cint(self.home_club) + cint(self.other_club) + cint(self.dcm) \
+			+ cint(self.alumini) + cint(self.rotarians) + cint(self.pis) + cint(self.guest)
+
+	def set_zone(self):
+		self.zone = frappe.db.get_value("Club", self.club, "zone")
