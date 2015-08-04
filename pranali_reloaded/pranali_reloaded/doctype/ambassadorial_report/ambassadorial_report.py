3374d8c7b81336ff56f20a68f08b9769b40d3bd8
Neil Trini Lasrado <neil@frappe.io>
2015-08-04 23:51:27 +0530

    Fixes for ambassodorial reports

--- pranali_reloaded/pranali_reloaded/doctype/ambassadorial_report/ambassadorial_report.py
+++ pranali_reloaded/pranali_reloaded/doctype/ambassadorial_report/ambassadorial_report.py
@@ -4,14 +4,18 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import now
 from frappe.model.document import Document
 
 class AmbassadorialReport(Document):
 	def validate(self):
 		self.set_zone()
 		self.document_status='draft'
+		self.check_duplicates()
+		self.count_total()
 
 	def on_submit(self):
+		self.time_stamp = now()
 		frappe.db.set_value('Ambassadorial Report', self.name, 'document_status', 'submitted')
 	
 	def on_cancel(self):
@@ -19,3 +23,16 @@ class AmbassadorialReport(Document):
 
 	def set_zone(self):
 		self.zone = frappe.db.get_value("Club", self.club, "zone")
+
+	def check_duplicates(self):
+		members= []
+		for d in self.members:
+			if not d.member_id in members:
+				members.append(d.member_id)
+			else:
+				frappe.throw("Attendence for {0} appears multiple times.".format(d.member_name))
+	
+	def count_total(self):
+		self.total_attendence = 0
+		for d in self.members:
+			self.total_attendence +=1
