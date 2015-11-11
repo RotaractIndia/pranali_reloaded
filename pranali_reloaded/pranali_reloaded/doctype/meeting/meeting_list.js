3f69e6b5c83c83565ab8107ef4cf38fced6a6ddd
Neil Trini Lasrado <neil@frappe.io>
2015-11-11 16:27:56 +0530

    Improved Meeting List view

---
+++ pranali_reloaded/pranali_reloaded/doctype/meeting/meeting_list.js
@@ -0,0 +1,20 @@
+frappe.listview_settings['Meeting'] = {
+	add_fields: ["type_of_meeting"],
+	get_indicator: function(doc) {
+		if (doc.type_of_meeting=="GBM") {
+			return [__("GBM"), "green", "type_of_meeting,=,GBM"];
+		}
+		else if (doc.type_of_meeting=="BOD Meet") {
+			return [__("BOD Meet"), "orange", "type_of_meeting,=,BOD Meet"];
+		}
+		else if (doc.type_of_meeting=="Joint Meet") {
+			return [__("Joint Meet"), "blue", "type_of_meeting,=,Joint Meet"];
+		}
+		else if (doc.type_of_meeting=="OCV") {
+			return [__("OCV"), "red", "type_of_meeting,=,OCV"];
+		}
+		else if (doc.type_of_meeting=="Any Other") {
+			return [__("Any Other"), "darkgrey", "type_of_meeting,=,Any Other"];
+		}
+	}
+};
