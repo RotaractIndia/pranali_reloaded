5ba75aa51c5b6b0333ecf8ee0a583078b8662dc5
Neil Trini Lasrado <neil@frappe.io>
2015-11-12 18:27:40 +0530

    Fixes in Club Stats

--- pranali_reloaded/pranali_reloaded/doctype/club/club.js
+++ pranali_reloaded/pranali_reloaded/doctype/club/club.js
@@ -1,19 +1,20 @@
 frappe.ui.form.on("Club", {
 	refresh: function(frm) {
 		if(!frm.doc.__islocal) {
-			frm.events.update_statistics(frm);
+			cur_frm.cscript.update_statistics(frm);
 		}
-	},
-	
-	update_statistics: function(frm) {
-		frappe.call({
-			method: "pranali_reloaded.pranali_reloaded.doctype.club.club.update_statistics",
-			args: {
-				"club": frm.doc.name
-			},
-			callback: function(r) {
-				if(!r.exc) frm.refresh_fields();
-			}
-		});
 	}
 });
+
+
+cur_frm.cscript.update_statistics= function(frm) {
+	frappe.call({
+		method: "pranali_reloaded.pranali_reloaded.doctype.club.club.update_statistics",
+		args: {
+			"club": frm.doc.name
+		},
+		callback: function(r) {
+			if(!r.exc) frm.refresh_fields();
+		}
+	});
+}
