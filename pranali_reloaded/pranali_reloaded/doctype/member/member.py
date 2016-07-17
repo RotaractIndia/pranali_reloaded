9c94acc7a7e1994a864b6eede18c4fe15fd63cf9
Neil Trini Lasrado <neil@frappe.io>
2016-07-17 13:48:51 +0530

    added receipt feature

--- pranali_reloaded/pranali_reloaded/doctype/member/member.py
+++ pranali_reloaded/pranali_reloaded/doctype/member/member.py
@@ -8,6 +8,16 @@ from frappe.model.document import Document
 class Member(Document):
 	def validate(self):
 		self.set_zone()
+		self.validate_dues()
 		
 	def set_zone(self):
 		self.zone = frappe.db.get_value("Club", self.club, "zone")
+		
+	def validate_dues(self):
+		if not self.dues_paid:
+			balance_amount = frappe.db.get_value("Club", "balance_amount")
+			if balance_amount < 50:
+				frappe.throw("You cannot add a new Member")
+			else:
+				self.dues_paid = 1
+				frappe.db.set_value("Club", self.club, "balance_amount", balance_amount)
