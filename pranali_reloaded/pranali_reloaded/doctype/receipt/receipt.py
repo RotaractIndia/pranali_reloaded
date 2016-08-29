8568301497949ad0b59617f65b3ec58c0297e350
Neil Trini Lasrado <neil@frappe.io>
2016-08-29 16:28:41 +0530

    fixes for receipt

--- pranali_reloaded/pranali_reloaded/doctype/receipt/receipt.py
+++ pranali_reloaded/pranali_reloaded/doctype/receipt/receipt.py
@@ -8,20 +8,26 @@ from frappe.model.document import Document
 from frappe.utils import now
 
 class Receipt(Document):
+	def validate(self):
+		if not self.club:
+			self.title = self.receivers_name
+			
 	def on_submit(self):
 		self.date = now()
-		if self.credit_amount:
-			balance_amount = frappe.db.get_value("Club", self.club, "balance_amount")
-			frappe.db.set_value("Club", self.club, "balance_amount", balance_amount + self.amount)
+		if self.club:
+			if self.credit_amount:
+				balance_amount = frappe.db.get_value("Club", self.club, "balance_amount")
+				frappe.db.set_value("Club", self.club, "balance_amount", balance_amount + self.amount)
 		
-		total_amount = frappe.db.get_value("Club", self.club, "total_amount")
-		frappe.db.set_value("Club", self.club, "total_amount", total_amount + self.amount)
+			total_amount = frappe.db.get_value("Club", self.club, "total_amount")
+			frappe.db.set_value("Club", self.club, "total_amount", total_amount + self.amount)
 		
 	def on_cancel(self):
-		if self.credit_amount:
-			balance_amount = frappe.db.get_value("Club", self.club, "balance_amount")
-			frappe.db.set_value("Club", self.club, "balance_amount", balance_amount - self.amount)
+		if self.club:
+			if self.credit_amount:
+				balance_amount = frappe.db.get_value("Club", self.club, "balance_amount")
+				frappe.db.set_value("Club", self.club, "balance_amount", balance_amount - self.amount)
 		
-		total_amount = frappe.db.get_value("Club", self.club, "total_amount")
-		frappe.db.set_value("Club", self.club, "total_amount", total_amount - self.amount)
+			total_amount = frappe.db.get_value("Club", self.club, "total_amount")
+			frappe.db.set_value("Club", self.club, "total_amount", total_amount - self.amount)
 		
