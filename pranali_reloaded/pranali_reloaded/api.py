0f8ff230d7c9e098231e4f20cccc083c91504745
Neil Trini Lasrado <neil@frappe.io>
2016-09-16 16:49:07 +0530

    fixes

--- pranali_reloaded/pranali_reloaded/api.py
+++ pranali_reloaded/pranali_reloaded/api.py
@@ -1,9 +1,11 @@
 import frappe
+from frappe.utils import cint
 
 def recalculate_balance_amount():
 	for c in frappe.get_list("Club"):
 		club = frappe.get_doc("Club", c)
 		if club.total_amount:
-			members = frappe.db.sql("select count(name) from tabMember where club=%s", c.name)[0][0]		
-			club.balance_amount = club.total_amount - (members*50)
+			members = frappe.db.sql("select count(name) from tabMember where club=%s", c.name)[0][0]
+			credit_amount = frappe.db.sql("select sum(amount) from tabReceipt where club=%s and credit_amount=1", c.name)[0][0]	
+			club.balance_amount = cint(credit_amount) - ( cint(members)*50)
 			club.save()
