ed9ddabdfef5663df2f14cc9090d0184f33e6fba
Neil Trini Lasrado <neil@frappe.io>
2016-10-30 00:03:37 +0530

    Added pointing system to ocv

--- pranali_reloaded/pranali_reloaded/doctype/ocv/ocv.py
+++ pranali_reloaded/pranali_reloaded/doctype/ocv/ocv.py
@@ -7,4 +7,32 @@ import frappe
 from frappe.model.document import Document
 
 class OCV(Document):
-	pass
+	def validate(self):
+		self.points = 0
+		print frappe.utils.now()
+		if frappe.utils.now() < "2017-03-01":
+			self.points = 5000
+		if self.gavel:
+			self.points += 300
+		if self.charter:
+			self.points += 1500
+		if self.collar:
+			self.points += 1000
+		if self.saa:
+			self.points += 500
+		if self.minutes:
+			self.points += 750
+		if self.attendance:
+			self.points += 750
+		if self.banner:
+			self.points += 500
+		if self.rooster:
+			self.points += 500
+		if self.website:
+			self.points += 500
+		if self.membership:
+			self.points += 1500
+		if self.bye_laws:
+			self.points += 3000
+		if self.finance:
+			self.points += 3000
