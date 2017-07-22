5c5c1f9b6bb6e1cd87e320de86448652754e9f7b
Neil Trini Lasrado <neil@frappe.io>
2017-07-22 14:18:06 +0530

    Added Status to Member

--- pranali_reloaded/pranali_reloaded/doctype/member/member.js
+++ pranali_reloaded/pranali_reloaded/doctype/member/member.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on('Member', {
     refresh: function(frm) {
-        if (!frm.doc.dues_paid && !frm.is_new() && frm.perm[0].write) {
+        if (!frm.doc.dues_paid && !frm.is_new() && frm.perm[0].write && frm.doc.status == "Active Rotaractor") {
             frm.add_custom_button(__("Re-Register Member"), function() {
                 frappe.call({
                     method: "pranali_reloaded.pranali_reloaded.doctype.member.member.register_member",
