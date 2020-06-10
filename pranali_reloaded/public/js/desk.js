11630bc339ec3881278672086d947501c832c78a
Neil Lasrado <neil@digithinkit.com>
2020-06-10 15:41:26 +0530

    Fixed an issue with desk not loading

--- pranali_reloaded/public/js/desk.js
+++ pranali_reloaded/public/js/desk.js
@@ -3,6 +3,37 @@ import Desktop from './components/Desktop.vue';
 frappe.provide('frappe.views.pageview');
 
 frappe.views.pageview = {
+	with_page: function(name, callback) {
+		if(in_list(Object.keys(frappe.standard_pages), name)) {
+			if(!frappe.pages[name]) {
+				frappe.standard_pages[name]();
+			}
+			callback();
+			return;
+		}
+
+		if((locals.Page && locals.Page[name] && locals.Page[name].script) || name==window.page_name) {
+			// already loaded
+			callback();
+		} else if(localStorage["_page:" + name] && frappe.boot.developer_mode!=1) {
+			// cached in local storage
+			frappe.model.sync(JSON.parse(localStorage["_page:" + name]));
+			callback();
+		} else {
+			// get fresh
+			return frappe.call({
+				method: 'frappe.desk.desk_page.getpage',
+				args: {'name':name },
+				callback: function(r) {
+					if(!r.docs._dynamic_page) {
+						localStorage["_page:" + name] = JSON.stringify(r.docs);
+					}
+					callback();
+				},
+				freeze: true,
+			});
+		}
+	},
 	show: function(name) {
 		if(!name) {
 			name = (frappe.boot ? frappe.boot.home_page : window.page_name);
