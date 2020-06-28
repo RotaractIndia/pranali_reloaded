8b0af11f38fefef551e6d9b04736e404895744c0
Neil Lasrado <neil@digithinkit.com>
2020-06-28 21:36:05 +0530

    [fix] Fixed issue with setting DCM user permssions

--- pranali_reloaded/hook_events/user.py
+++ pranali_reloaded/hook_events/user.py
@@ -2,7 +2,7 @@ import frappe
 
 def set_user_permissions_for_dcm(doc, method):
     for r in doc.roles:
-        if r.role =="DCM":
+        if r.role =="District Council Member":
             if not frappe.get_all("User Permission",filters={"user":doc.name}):
                 perm = frappe.new_doc("User Permission")
                 perm.user = doc.name
