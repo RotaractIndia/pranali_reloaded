efa2548d30eadf4477b7cbcdd62cd6aff8c495f4
Neil Lasrado <nlasrado@gmail.com>
2021-04-25 13:29:48 +0530

    [feat] Added Rotaract Year

---
+++ pranali_reloaded/pranali_reloaded/doctype/rotaract_year/rotaract_year.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Rtr.Neil Trini Lasrado and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Rotaract Year', {
+	// refresh: function(frm) {
+
+	// }
+});
