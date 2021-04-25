8da1e4cc41d2fff6bdd841f0c0dee8a8b6e7f653
Neil Lasrado <nlasrado@gmail.com>
2021-04-25 17:02:39 +0530

    [fix] Fixed move_registrations_to_member patch

--- pranali_reloaded/patches/move_registrations_to_member.py
+++ pranali_reloaded/patches/move_registrations_to_member.py
@@ -2,12 +2,11 @@ import frappe
 
 def execute():
     frappe.reload_doc('pranali_reloaded', 'doctype', 'member', force=True)
-    users = frappe.get_all("Registrations")
+    users = registration = frappe.db.sql("select * from `tabRegistrations`", as_dict=True)
     for user in users:
-        registration = frappe.get_doc("Registrations", user.name)
-        member = frappe.get_all("Member", filters={"email": user.name})
+        member = frappe.get_all("Member", filters={"email": user.get('name')})
         if member:
-            frappe.db.set_value("Member", member[0].name, "user", registration.user)
-            frappe.db.set_value("Member", member[0].name, "enable_pranali_access", registration.enabled)
-            frappe.db.set_value("Member", member[0].name, "limited_access", registration.limited_access)
+            frappe.db.set_value("Member", member[0].name, "user", user.get('name'))
+            frappe.db.set_value("Member", member[0].name, "enable_pranali_access", user.get('enabled'))
+            frappe.db.set_value("Member", member[0].name, "limited_access", user.get('limited_access'))
             
