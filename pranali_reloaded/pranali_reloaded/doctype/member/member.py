22831fc0823e4cbb758967190364c15da875dd54
Neil Lasrado <nlasrado@gmail.com>
2021-04-25 16:36:42 +0530

    [Feat] Merged Regsitrations into Member

--- pranali_reloaded/pranali_reloaded/doctype/member/member.py
+++ pranali_reloaded/pranali_reloaded/doctype/member/member.py
@@ -9,6 +9,7 @@ from frappe.utils import flt
 class Member(Document):
 	def validate(self):
 		self.set_zone()
+		self.validate_pranali_access()
 		self.member_name = self.member_name.title()
 
 	def before_insert(self):
@@ -25,6 +26,74 @@ class Member(Document):
 		balance_amount = frappe.db.get_value("Club", self.club, "balance_amount")
 		if balance_amount < district_dues:
 			frappe.throw("Insufficient funds! You cannot add a new Member")
+	
+	def validate_pranali_access(self):
+		if not self.email:
+			self.disable_user()
+		elif self.user and self.user != self.email:
+			self.rename_user()
+		
+		if not self.user and self.email and self.enable_pranali_access:
+			if not frappe.db.exists("User", self.email):
+				self.make_user()
+			else:
+				self.user = self.email
+		elif self.user and self.email:
+			self.update_user()
+		
+	def make_user(self):
+		user = frappe.new_doc("User")
+		user.update({
+			"first_name": self.member_name,
+			"email": self.email,
+			"mobile_no": self.contact_number,
+			"send_welcome_email": 1
+		})
+		user.save(ignore_permissions=True)
+		user.update({
+			"roles": [
+				{"role": "Club Member - Limited Access"}
+			],
+			"bio": self.club
+		})
+		user.save(ignore_permissions=True)
+		self.user = user.name
+		self.limited_access=True
+		permission = frappe.new_doc("User Permission")
+		permission.user = user.name
+		permission.allow = "Club"
+		permission.for_value = self.club
+		permission.save(ignore_permissions=True)
+
+	def update_user(self):
+		user=frappe.get_doc("User", self.user)
+		user.enabled=self.enable_pranali_access
+		user.email = self.email
+		user.mobile_no = self.contact_number
+		if self.limited_access:
+			user.update({
+				"roles": [
+					{"role": "Club Member - Limited Access"}
+				]
+			})
+		else:
+			user.update({
+				"roles": [
+					{"role": "Club Member - Full Access"}
+				]
+			})
+		user.save(ignore_permissions=True)
+
+	def rename_user(self):
+		frappe.rename_doc("User", self.user, self.email)
+		self.user = self.email
+
+	def disable_user(self):
+		self.enable_pranali_access = False
+		user=frappe.get_doc("User", self.user)
+		user.enabled=self.enable_pranali_access
+		user.save(ignore_permissions=True)
+		self.user = None
 
 	def on_trash(self):
 		club = frappe.get_doc("Club", self.club)
