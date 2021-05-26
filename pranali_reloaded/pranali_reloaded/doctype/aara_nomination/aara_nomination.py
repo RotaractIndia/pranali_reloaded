52dceb2850446e6e10d038837af34f18d548a4db
Neil Lasrado <nlasrado@gmail.com>
2021-05-26 17:55:45 +0530

    [feat] Added Yearly nominations to AARAS

--- pranali_reloaded/pranali_reloaded/doctype/aara_nomination/aara_nomination.py
+++ pranali_reloaded/pranali_reloaded/doctype/aara_nomination/aara_nomination.py
@@ -10,11 +10,16 @@ class AARANomination(Document):
 	def validate(self):
 		avenue_list = {}
 		joint = 0
-		ongoing = 0
-		flagship = 0 
 		nomination_avenue = []
 		self.rotaract_year = frappe.db.get_single_value("Pranali Settings", "current_rotaract_year")
 		limits = get_avenue_limit()
+		yearly_limits = get_yearly_limit()
+
+		if self.quarter == "Yearly":
+			self.projects = []
+			print(self.projects)
+		else:
+			self.yearly_nomination = []
 		
 		if self.quarter == "One":
 			nomination_avenue = [ "July", "August", "September"]
@@ -27,41 +32,31 @@ class AARANomination(Document):
 
 		for nomination in self.projects:
 			self.validate_reporting_status(nomination)
-			
-			if nomination.nominate_for == "Ongoing":
-				if ongoing==2:
-					frappe.throw("You cannot nominate more than 2 ongoing Projects in a quarter")
+			reporting_month = frappe.db.get_value("Project", nomination.project, "reporting_month")
+			if reporting_month not in nomination_avenue:
+				frappe.throw("You cannot Nominate {0} - {1} for Quarter {2} as it was reported in the month of {3}"
+					.format(nomination.project, nomination.project_name, self.quarter, reporting_month))
+						
+			if nomination.nominate_for == "Joint":
+				if joint==2:
+						frappe.throw("You cannot nominate more than 2 Joint Projects in a quarter")
 				else:
-					ongoing = ongoing + 1
-			elif nomination.nominate_for == "Flagship":
-				if flagship==2:
-						frappe.throw("You cannot nominate more than 2 flagship Projects in a quarter")
-				else:
-					flagship = flagship + 1
+					joint = joint + 1
 			else:
-				reporting_month = frappe.db.get_value("Project", nomination.project, "reporting_month")
-				if reporting_month not in nomination_avenue:
-					frappe.throw("You cannot Nominate {0} - {1} for Quarter {2} as it was reported in the month of {3}"
-						.format(nomination.project, nomination.project_name, self.quarter, reporting_month))
-									
-				if nomination.nominate_for == "Joint":
-					if joint==2:
-							frappe.throw("You cannot nominate more than 2 Joint Projects in a quarter")
-					else:
-						joint = joint + 1
-				else:
-					nomination.avenue = frappe.db.get_value("Project", nomination.project, frappe.scrub(nomination.nominate_for))
-					if frappe.db.get_value("Project", nomination.project, "joint_project"):
-						frappe.throw("You cannot Nominate {0} - {1} since its a Joint Project".format(nomination.project, nomination.project_name))
-					
-					if not nomination.avenue in avenue_list.keys():
-						avenue_list.update({nomination.avenue: 1})
-					else:
-						if avenue_list.get(nomination.avenue)==limits.get(nomination.avenue):
-							frappe.throw("You cannot nominate more that {0} Projects under Avenue {1} for Quarter {2}".format(limits.get(nomination.avenue), nomination.avenue, self.quarter))
-						else:
-							avenue_list.update({nomination.avenue: 2})
+				nomination.avenue = frappe.db.get_value("Project", nomination.project, frappe.scrub(nomination.nominate_for))
+				if frappe.db.get_value("Project", nomination.project, "joint_project"):
+					frappe.throw("You cannot Nominate {0} - {1} since its a Joint Project".format(nomination.project, nomination.project_name))
+				
+				avenue_list.update({nomination.avenue: avenue_list.get(nomination.avenue, 0) +1})
+				if avenue_list.get(nomination.avenue)>limits.get(nomination.avenue):
+					frappe.throw("You cannot nominate more that {0} Projects under Avenue {1} for Quarter {2}".format(limits.get(nomination.avenue), nomination.avenue, self.quarter))					
 
+		for nomination in self.yearly_nomination:
+			self.validate_reporting_status(nomination)
+			avenue_list.update({nomination.nomination_category: avenue_list.get(nomination.nomination_category, 0) +1})
+			if avenue_list.get(nomination.nomination_category)>yearly_limits.get(nomination.nomination_category):
+				frappe.throw("You cannot nominate more that {0} Projects under Nomination Category {1}".format(yearly_limits.get(nomination.nomination_category), nomination.nomination_category))
+	
 	def before_submit(self):
 		rotaract_year = frappe.db.get_single_value("Pranali Settings", "current_rotaract_year")
 		existing_nomination = frappe.get_all("AARA Nomination", filters={"Club": self.club, "Quarter": self.quarter, "docstatus": 1, "rotaract_year": rotaract_year})
@@ -78,6 +73,13 @@ def get_avenue_limit():
 		limits[avenue.get('name')] = avenue.get('nominations_per_quarter')
 	return limits
 
+def get_yearly_limit():
+	limits = {}
+	for category in frappe.get_all("Nomination Category", fields=["name", "nominations_per_year"]):
+		limits[category.get('name')] = category.get('nominations_per_year')
+	return limits
+
 @frappe.whitelist()
 def get_nomination_avenue(project=None, avenue=None):
-	return frappe.db.get_value("Project", project, frappe.scrub(avenue))
+	if avenue:
+		return frappe.db.get_value("Project", project, frappe.scrub(avenue))
