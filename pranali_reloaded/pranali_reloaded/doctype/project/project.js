cea58101e5a84d57897bbcc4b26343e6b35e8000
Neil Lasrado <nlasrado@gmail.com>
2021-08-07 03:18:47 +0530

    [feat] Added Othere District to Project reporting

--- pranali_reloaded/pranali_reloaded/doctype/project/project.js
+++ pranali_reloaded/pranali_reloaded/doctype/project/project.js
@@ -1,11 +1,11 @@
 cur_frm.cscript.guest = function(doc) {
 	doc.total = cint(doc.home_club) + cint(doc.other_club) + cint(doc.dcm) 
-			+ cint(doc.alumini) + cint(doc.rotarians) + cint(doc.pis) + cint(doc.guest)
+			+ cint(doc.alumini) + cint(doc.rotarians) + cint(doc.pis) + cint(doc.guest) + cint(doc.other_district) 
 	refresh_field('total');
 };
 
 cur_frm.cscript.home_club = cur_frm.cscript.other_club = cur_frm.cscript.dcm = cur_frm.cscript.alumini =
-	cur_frm.cscript.rotarians = cur_frm.cscript.pis = cur_frm.cscript.guest;
+	cur_frm.cscript.rotarians = cur_frm.cscript.pis = cur_frm.cscript.other_district = cur_frm.cscript.guest;
 
 frappe.ui.form.on('Project', {
     onload: (frm) => {
